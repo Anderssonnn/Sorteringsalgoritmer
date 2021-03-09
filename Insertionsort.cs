d72cab35d639e99c21502171f4c86f11f5b374c0
Anderssonnn <69970038+Anderssonnn@users.noreply.github.com>
2021-03-09 19:50:36 +0100

    la upp fil

---
+++ Insertionsort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static double[] randomArray = new Double[10000];
+
+    static void RandomNumbers()
+    {
+        Random randomNumber = new Random();
+        for (int i = 0; i < randomArray.GetLength(0); i++)
+        {
+            randomArray[i] = randomNumber.NextDouble();
+        }
+    }
+
+    static void InsertionSort()
+    {
+        double temp;
+        int inner;
+        for (int outer = 0; outer < randomArray.GetLength(0); outer++)
+        {
+            temp = randomArray[outer];
+            inner = outer;
+            while (inner > 0 && (randomArray[inner - 1] >= temp))
+            {
+                randomArray[inner] = randomArray[inner - 1];
+                inner -= 1;
+            }
+            randomArray[inner] = temp;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        RandomNumbers();
+        Console.WriteLine("Number of Items: {0}", randomArray.GetLength(0));
+        Stopwatch sWatch = new Stopwatch();
+        sWatch.Reset();
+        sWatch.Start();
+        InsertionSort();
+        sWatch.Stop();
+        Console.WriteLine("Insertion Sort Completed: {0} milliseconds", sWatch.Elapsed);
+        Console.ReadLine();
+    }
+}
